7b0aef52983d52b001a37473fca32513a4a4a3b2
Sheena Bonilla <sheena.myb@gmail.com>
2022-08-22 13:13:40 -0700

    commit tree fed114b10260b1348d96f3342af68c9bfe34fc4b

--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -4,7 +4,9 @@ import Todo from './Todo';
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+  // key = randomly generated ID
+  // passing the todo object as a prop rather than just text
+  renderTodos = () => this.props.todos.map((todo) => <Todo delete={this.props.delete} key={todo.id} text={todo} />)
   
   render() {
     return(
@@ -20,5 +22,11 @@ class TodosContainer extends Component {
       todos: state.todos
     }
   }
+
+  const mapDispatchToProps = dispatch => {
+    return {
+      delete: (todoText) => dispatch({type: 'DELETE_TODO', payload: todoText})
+    };
+  };
   
-  export default connect(mapStateToProps)(TodosContainer);
+  export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
